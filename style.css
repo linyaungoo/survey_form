d9c876a643ddecb4978039553bc952d34a0b4b45
Lin Yaung Oo <32572390+linyaungoo@users.noreply.github.com>
2022-12-01 11:41:20 +0630

    Initial Commit

---
+++ style.css
@@ -0,0 +1,82 @@
+body{
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  min-height: 100vh;
+  background: #5372F0
+}
+.wrapper{
+  background: #fff;
+  width: 380px;
+  text-align: center;
+  padding: 40px 30px 50px 30px;
+  border-radius: 5px;
+}
+.wrapper header{
+  font-size: 35px;
+  font-weight: 600;
+}
+.wrapper form{
+  margin: 40px 0;
+}
+form input{
+  height: 50px;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  border-radius: 5px;
+  border-bottom-width: 2px;
+  border: 1px solid #fbfbfb;
+}
+form .field{
+  margin-bottom: 20px;
+  width: 100%;
+
+}
+form .field .shake{
+  animation: shake 0.3s ease-in-out infinite;
+}
+@keyframes shake {
+  0%, 100%{
+    margin-left: 0;
+  }
+  20%, 80%{
+    margin-left: -12px;
+  }
+  40%, 60%{
+    margin-left: 12px;
+  }
+}
+form .field .error{
+  display: none;
+}
+form .field .input_area{
+  position: relative;
+}
+form .field .error-text{
+  margin: 5px;
+  color: #dc3545;
+  text-align: left;
+}
+form .pass-link{
+  text-align: left;
+  margin-top: -10px;
+}
+.wrapper a{
+  color: #5372F0;
+  text-decoration: none;
+}
+.wrapper a:hover{
+  text-decoration: underline;
+}
+form input[type="Submit"]{
+  margin-top: 30px;
+  border: none;
+  color:#fff ;
+  cursor: pointer;
+  background: #5372F0;
+  transition: background 0.2s ease;
+}
+input[type="Submit"]:hover{
+  background: #2c52ed;
+}
